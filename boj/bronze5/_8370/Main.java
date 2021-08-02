5a4928ccb1c2a69057875d328cb813e8be28b5cd
GoAdRider <dhtbfk3425@naver.com>
2021-08-02 20:40:47 +0900

    complete 8370

---
+++ boj/bronze5/_8370/Main.java
@@ -0,0 +1,26 @@
+package _8370;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+	public static void main(String[] args) throws IOException { 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] plane = new int[4];
+		
+		for(int i=0;i<4;i++) {
+			plane[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		bw.write(String.valueOf((plane[0]*plane[1])+(plane[2]*plane[3])));
+		bw.flush(); 
+		bw.close();
+	}
+}
