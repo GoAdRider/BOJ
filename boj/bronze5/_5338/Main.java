42f03165422634457fe7c4feb0dac568f61b2928
GoAdRider <dhtbfk3425@naver.com>
2021-08-02 19:33:36 +0900

    5338 complete

---
+++ boj/bronze5/_5338/Main.java
@@ -0,0 +1,30 @@
+package _5338;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/*
+마이크로소프트 로고를 예제 출력처럼 출력하는 프로그램을 작성하시오.
+ 
+ --예제출력
+       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|
+
+
+ */
+
+public class Main {
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+	public static void main(String[] args) throws IOException { 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		bw.write("       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|");
+		bw.flush(); 
+		bw.close();
+	}
+}
