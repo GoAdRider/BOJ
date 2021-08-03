3a14252eaff4aee5c3bd31a2c47408f57f4cdda3
GoAdRider <dhtbfk3425@naver.com>
2021-08-03 15:45:28 +0900

    10757 complete

---
+++ boj/bronze5/_10757/Main.java
@@ -0,0 +1,38 @@
+package _10757;
+
+
+/*
+큰수 A+B
+
+
+예제 입력---
+9223372036854775807 9223372036854775808
+
+예제 출력---
+18446744073709551615
+
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+	public static void main(String[] args) throws IOException { 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+
+		bw.write(String.valueOf(A.add(B)));
+		bw.flush(); 
+		bw.close();
+        
+    }
+}
+
