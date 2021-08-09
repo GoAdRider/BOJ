6288026410dc02d4dcfaac9571e52fcabef6013b
GoAdRider <dhtbfk3425@naver.com>
2021-08-09 23:30:23 +0900

    1712 complete

---
+++ boj/bronze4/_1712/Main.java
@@ -0,0 +1,30 @@
+package _1712;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	public static void main(String[] args) throws IOException { 
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+ 
+		if (C <= B) {
+			bw.write(String.valueOf("-1"));
+			bw.flush(); 
+		} 
+		else {
+			bw.write(String.valueOf((A/(C-B))+1));
+			bw.flush(); 
+		}
+		bw.close();
+	}
+}
