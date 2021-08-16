b857ccd9e19b62520050a32ba30cbe2b52f92886
GoAdRider <dhtbfk3425@naver.com>
2021-08-17 00:48:00 +0900

    10797 complete

---
+++ boj/bronze4/_10797/Main.java
@@ -0,0 +1,28 @@
+package boj.bronze4._10797;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	private static StringBuilder sb = new StringBuilder();	
+    public static void main(String[] args) throws IOException {
+        int count=0;
+        int A = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()){
+            if(A==Integer.parseInt(st.nextToken())){
+                count++;
+            }
+        }
+        sb.append(count);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
