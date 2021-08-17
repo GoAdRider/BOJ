d746c352d9126cce9c88a70e1b3622862878d907
GoAdRider <dhtbfk3425@naver.com>
2021-08-17 13:35:21 +0900

    11282 complete

---
+++ boj/bronze4/_11282/Main.java
@@ -0,0 +1,19 @@
+package boj.bronze4._11282;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	private static StringBuilder sb = new StringBuilder();	
+    public static void main(String[] args) throws IOException {
+        sb.append((char)(Integer.parseInt(br.readLine())+44031));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
