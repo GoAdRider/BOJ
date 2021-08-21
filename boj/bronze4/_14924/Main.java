9dd9bcefde361b0f0536c45a6ef297cbbd9b8575
GoAdRider <dhtbfk3425@naver.com>
2021-08-21 16:33:33 +0900

    14924 complete

---
+++ boj/bronze4/_14924/Main.java
@@ -0,0 +1,28 @@
+package bronze4._14924;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	private static StringBuilder sb = new StringBuilder();	
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        int ans=D/(S*2);
+
+        sb.append(ans*T);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
