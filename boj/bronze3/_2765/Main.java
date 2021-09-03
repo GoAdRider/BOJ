3e203150f98bdb0c888bedfcd14f4e0cc5f7ecbb
GoAdRider <osk3856@gmail.com>
2021-09-03 19:55:03 +0900

    2765 complete

---
+++ boj/bronze3/_2765/Main.java
@@ -0,0 +1,39 @@
+package bronze3._2765;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+    private static StringBuffer sb = new StringBuffer();	
+    private static final double pi = 3.1415927;
+    public static void main(String[] args) throws IOException {
+        String str = "";
+        int count = 1;
+        //Math.round() 
+        while ((str=br.readLine())!=null) {
+            double mile = 0;
+            double mph = 0;
+            StringTokenizer st = new StringTokenizer(str);
+            double round = Double.parseDouble(st.nextToken())*pi;// 단위는 인치 // 1피트 = 12 인치 // 1마일 = 5280피트
+            int rotate = Integer.parseInt(st.nextToken());
+            if(rotate==0)
+                break;
+            double second = Double.parseDouble(st.nextToken());
+
+            mile = (round*rotate)/63360;
+            mph = (mile/second)*3600;
+
+            sb.append("Trip #").append(count+": ").append(String.format("%.2f ",mile)).append(String.format("%.2f\n",mph));
+            count++;
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
