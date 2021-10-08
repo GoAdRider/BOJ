bcd66744f1f433dbcd8d9ee1e08c4726bb588646
GoAdRider <osk3856@gmail.com>
2021-10-09 06:56:31 +0900

    2443 complete

---
+++ boj/bronze3/_2443/Main.java
@@ -0,0 +1,28 @@
+package bronze3._2443;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	private static StringBuilder sb = new StringBuilder();	
+    public static void main(String[] args) throws IOException {
+        int size = Integer.parseInt(br.readLine());
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j <= i-1; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < (size*2)-((i*2)+1); j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
