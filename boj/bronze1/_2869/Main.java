c5cb0495484ddf0e45c68e4d2b4826317d0714db
GoAdRider <osk3856@gmail.com>
2021-10-26 18:22:55 +0900

    2869 complete

---
+++ boj/bronze1/_2869/Main.java
@@ -0,0 +1,45 @@
+package bronze1._2869;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/*
+땅 위에 달팽이가 있다. 이 달팽이는 높이가 V미터인 나무 막대를 올라갈 것이다.
+
+달팽이는 낮에 A미터 올라갈 수 있다. 하지만, 밤에 잠을 자는 동안 B미터 미끄러진다. 또, 정상에 올라간 후에는 미끄러지지 않는다.
+
+달팽이가 나무 막대를 모두 올라가려면, 며칠이 걸리는지 구하는 프로그램을 작성하시오.
+*/
+
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int walk = 0;
+        int day = 1;
+
+        if(V-A>0){
+            int x = V-A;
+            walk = A-B;
+            day += x/walk;
+            if(x%walk != 0){
+                day ++;
+            }
+        }
+
+        bw.write(String.valueOf(day));
+        bw.flush();
+        bw.close();
+    }
+}
