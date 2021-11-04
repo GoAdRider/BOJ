3462041f2ed9728bcd01d0f57b1caf5b934e3e6e
GoAdRider <osk3856@gmail.com>
2021-11-04 22:19:53 +0900

    단어 뒤집기2 준비중

---
+++ boj/basic/datastructure201/_17413/Main.java
@@ -0,0 +1,15 @@
+package basic.datastructure201._17413;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+    }
+}
