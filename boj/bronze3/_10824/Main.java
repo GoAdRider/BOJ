2c8c08e41a88eec7f094aeff327b85d30342d56d
GoAdRider <osk3856@gmail.com>
2021-11-17 23:55:57 +0900

    10824 네수 complete

---
+++ boj/bronze3/_10824/Main.java
@@ -0,0 +1,25 @@
+package bronze3._10824;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 네 수
+
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+    public static void main(String[] args) throws NumberFormatException, IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long n1 = Long.valueOf(st.nextToken() + st.nextToken());
+		long n2 = Long.valueOf(st.nextToken() + st.nextToken());
+
+        bw.write(String.valueOf(n1+n2));
+        bw.flush();
+        bw.close();
+    }
+}
