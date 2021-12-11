01b9d92f9c2985f66682825c6e89fd99a94ced3d
GoAdRider <osk3856@gmail.com>
2021-12-11 22:26:07 +0900

    그대로 출력하기 complete

---
+++ boj/bronze3/_11718/Main.java
@@ -0,0 +1,21 @@
+package bronze3._11718;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// 그대로 출력하기
+
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        String str="";
+        while((str=br.readLine()) != null){
+            System.out.println(str);
+        }
+    }
+}
