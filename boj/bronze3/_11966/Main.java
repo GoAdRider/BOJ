4ca14ed0cf6e5f550e9fb4b88180abf2102abfac
GoAdRider <osk3856@gmail.com>
2021-12-15 20:42:49 +0900

    2의 제곱인가? 비트표현 풀이 complete

---
+++ boj/bronze3/_11966/Main.java
@@ -0,0 +1,22 @@
+package bronze3._11966;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        if(n == (n&-n)) bw.write(String.valueOf(1));
+        else bw.write(String.valueOf(0));
+
+        bw.flush();
+        bw.close();
+    }
+}
