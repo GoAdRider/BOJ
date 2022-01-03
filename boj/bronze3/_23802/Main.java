13c87dc17e42f11aaf388f080d5ff4ab6b1a73d6
GoAdRider <osk3856@gmail.com>
2022-01-03 23:17:52 +0900

    골뱅이 찍기 - 뒤집힌 ㄱ complete

---
+++ boj/bronze3/_23802/Main.java
@@ -0,0 +1,30 @@
+package bronze3._23802;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// 골뱅이 찍기 - 뒤집힌 ㄱ
+
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException  {
+        int size = Integer.parseInt(br.readLine());
+        for (int i = 0; i < size*5; i++) {
+            for (int j = 0; j < size*5; j++) {
+                if(i<size || j<size){
+                    sb.append("@");
+                }
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
