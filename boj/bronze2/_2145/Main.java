5d0bd4399af487389b7d1857f470aac4bd8cfcd4
GoAdRider <osk3856@gmail.com>
2022-01-25 13:48:52 +0900

    숫자놀이 complete

---
+++ boj/bronze2/_2145/Main.java
@@ -0,0 +1,38 @@
+package bronze2._2145;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// 숫자 놀이
+
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args)throws IOException  {
+        int n;
+
+        while((n=Integer.parseInt(br.readLine()))!=0){
+            sb.append(calc(n)).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    static int calc(int n){
+        int sum=0;
+        while(n/10 != 0){
+            sum += n%10;
+            n/=10;
+        }
+        sum+=n;
+
+        if(sum/10 == 0) return sum;
+        else return calc(sum);
+    }
+}
