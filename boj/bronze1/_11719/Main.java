2f223982089f400a205b57c926645a41c0144d87
GoAdRider <osk3856@gmail.com>
2022-03-22 19:39:41 +0900

    그대로 출력하기2 complete

---
+++ boj/bronze1/_11719/Main.java
@@ -0,0 +1,19 @@
+package bronze1._11719;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        String str;
+        while((str=br.readLine())!=null){
+            System.out.println(str);
+        }
+    }
+}
