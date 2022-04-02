510d20b62af8b88934230bb254c637d672cf5f48
GoAdRider <osk3856@gmail.com>
2022-04-03 00:00:28 +0900

    고급 여관 complete

---
+++ boj/bronze3/_12756/Main.java
@@ -0,0 +1,33 @@
+package bronze3._12756;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        String[] sA = br.readLine().split(" ");
+        int aA = Integer.parseInt(sA[0]);
+        int lA = Integer.parseInt(sA[1]);
+        sA = br.readLine().split(" ");
+        int aB = Integer.parseInt(sA[0]);
+        int lB = Integer.parseInt(sA[1]);
+        while(lA <=0 || lB<=0){
+            lA -= aB;
+            lB -= aA;
+        }
+        if(lA <= 0 && lB <= 0) sb.append("DRAW");
+        else{
+            if(lA <= 0) sb.append("PLAYER B");
+            else if(lB <= 0) sb.append("PLAYER A");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
